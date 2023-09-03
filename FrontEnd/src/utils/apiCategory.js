ad739fef56431f62fbf6d8dd01e141a9881278d9
ThuyTrang-IT <63693331+ThuyTrang-IT@users.noreply.github.com>
2023-09-03 16:17:49 +0700

    Update apiCategory.js

--- FrontEnd/src/utils/apiCategory.js
+++ FrontEnd/src/utils/apiCategory.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api/categories', 
+  baseURL: 'http://trangpham.gcall.vn/api/categories', 
 });
 
 export const fetchCategoryData = async () => {
